4b6ab681bd2de8899f36e29183f805b0585174f8
Chieu131 <142771810+Chieu131@users.noreply.github.com>
2023-11-24 08:35:34 +0700

    Create App.js

---
+++ item-list/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ItemCard from './component/ItemCard';
+import ItemList from './component/ItemList';
+function App(){
+    return(
+        <div className='app-container'>
+            {/* { <PricingPage/> } */}
+            <ItemList/>
+            
+        </div>
+    )
+ }
+ 
+ export default App;
