0b7253edcbbda60f4a2d2ad5b31cedadb90f9c95
Chieu131 <142771810+Chieu131@users.noreply.github.com>
2023-11-27 10:52:51 +0700

    Create Button.js

---
+++ React-js/src/chap04/Button.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Couters that update separately</h1>
+            <MyButton />
+            <MyButton />
+        </div>
+    );
+}
+
+function MyButton () {
+    const [count, setCount] = useState(0);
+
+    function handleClick (){
+        setCount(count+1);
+    }
+    return (
+        <button onClick={handleClick}>
+            Click {count} times
+        </button>
+    );
+}
